6315905b16e087a70a6bf1474121a0ae7e10932a
yiweihuang <n1993212@gmail.com>
2016-06-06 23:22:14 +0800

    ibnt

--- helper/file_helper.py
+++ helper/file_helper.py
@@ -2,14 +2,13 @@ import json
 
 
 def store_file(info, filename):
-    path = '../config/' + filename
+    path = './config/' + filename
     with open(path, 'w') as f:
         f.write(json.dumps(info))
-    f.close()
 
 
 def read_file(filename):
-    path = '../config/' + filename
+    path = './config/' + filename
     with open(path, 'r') as f:
         data = json.load(f)
 

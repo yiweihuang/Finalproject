ba1972a62e583e2d1dc9f3be7ca20cb42c39b52e
vicky <lch82327@gmail.com>
2016-06-07 17:55:27 +0800

    integreate hosname and ip function to one

--- packet_sender.py
+++ packet_sender.py
@@ -41,8 +41,7 @@ class PacketSender(app_manager.RyuApp):
         wsgi.register(PacketSenderController,
                       {packet_sender_instance_name: self})
 
-    # Hostname
-    def send_time_hostname(self):
+    def send_by_time(self):
         total = 0
         json_data = file_helper.read_file('target.json')
 
@@ -60,43 +59,7 @@ class PacketSender(app_manager.RyuApp):
                 print "Send one minute."
                 break
 
-    def send_count_hostname(self):
-        total = 0
-        json_data = file_helper.read_file('target.json')
-
-        ip = json_data['ip']
-        port = json_data['port']
-        count = json_data['count']
-
-        while 1:
-            if total < count:
-                sendSYN(ip, port).run()
-                total += 1
-                print total
-            else:
-                print "Send " + str(total) + " packet"
-                break
-
-    # IP address
-    def send_time_ip(self):
-        total = 0
-        json_data = file_helper.read_file('target.json')
-
-        ip = json_data['ip']
-        port = json_data['port']
-
-        runtime = time.time() + 60
-
-        while 1:
-            if time.time() < runtime:
-                sendSYN(ip, port).run()
-                total += 1
-                print total
-            else:
-                print "Send one minute."
-                break
-
-    def send_count_ip(self):
+    def send_by_count(self):
         total = 0
         json_data = file_helper.read_file('target.json')
 
@@ -120,60 +83,19 @@ class PacketSenderController(ControllerBase):
         super(PacketSenderController, self).__init__(req, link, data, **config)
         self.packet_sender_spp = data[packet_sender_instance_name]
 
-    # hostname
-    @route('packet_sender', urls.hostname_time, methods=['POST'])
-    def hn_sendpacket_bytime(self, req, **kwargs):
-        try:
-            packet_sender = self.packet_sender_spp
-            req = json.loads(req.body)
-
-            ip, port = dns_helper.translate_target(req['target'])
-            info = file_helper.info_builder(ip, port, req['count'])
-            file_helper.store_file(info, 'target.json')
-
-            packet_sender.send_time_hostname()
-        except:
-            return Response(status=500)
-
-    @route('packet_sender', urls.hostname_count, methods=['POST'])
-    def hn_sendpacket_bycount(self, req, **kwargs):
-        try:
-            packet_sender = self.packet_sender_spp
-            req = json.loads(req.body)
-
-            ip, port = dns_helper.translate_target(req['target'])
-            info = file_helper.info_builder(ip, port, req['count'])
-            file_helper.store_file(info, 'target.json')
-
-            packet_sender.send_count_hostname()
-        except:
-            return Response(status=500)
-
     # IP address
-    @route('packet_sender', urls.ip_time, methods=['POST'])
+    @route('packet_sender', urls.send_by_time, methods=['POST'])
     def ip_sendpacket_bytime(self, req, **kwargs):
         try:
             packet_sender = self.packet_sender_spp
-            req = json.loads(req.body)
-
-            ip, port = dns_helper.translate_target(req['target'])
-            info = file_helper.info_builder(ip, port, req['count'])
-            file_helper.store_file(info, 'target.json')
-
-            packet_sender.send_time_ip()
+            packet_sender.send_by_time()
         except:
             return Response(status=500)
 
-    @route('packet_sender', urls.ip_count, methods=['POST'])
+    @route('packet_sender', urls.send_by_count, methods=['POST'])
     def ip_sendpacket_bycount(self, req, **kwargs):
         try:
             packet_sender = self.packet_sender_spp
-            req = json.loads(req.body)
-
-            ip, port = dns_helper.translate_target(req['target'])
-            info = file_helper.info_builder(ip, port, req['count'])
-            file_helper.store_file(info, 'target.json')
-
-            packet_sender.send_count_ip()
+            packet_sender.send_by_count()
         except:
             return Response(status=500)
